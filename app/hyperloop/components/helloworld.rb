cb8e7d8cb439822c387533457353e17d4fd12023
Billy.Zheng <zw963@163.com>
2017-07-12 16:55:45 +0800

    按照 tutorial 重写 hello world!

--- app/hyperloop/components/helloworld.rb
+++ app/hyperloop/components/helloworld.rb
@@ -1,39 +1,39 @@
+class Helloworld < Hyperloop::Component
+  state show_field: false
+  state field_value: ''
 
-  class Helloworld < Hyperloop::Component
-
-    # param :my_param
-    # param param_with_default: "default value"
-    # param :param_with_default2, default: "default value" # alternative syntax
-    # param :param_with_type, type: Hash
-    # param :array_of_hashes, type: [Hash]
-    # collect_other_params_as :attributes  # collects all other params into a hash
-
-    # The following are the most common lifecycle call backs,
-    # the following are the most common lifecycle call backs# delete any that you are not using.
-    # call backs may also reference an instance method i.e. before_mount :my_method
-
-    before_mount do
-      # any initialization particularly of state variables goes here.
-      # this will execute on server (prerendering) and client.
-    end
-
-    after_mount do
-      # any client only post rendering initialization goes here.
-      # i.e. start timers, HTTP requests, and low level jquery operations etc.
-    end
-
-    before_update do
-      # called whenever a component will be re-rerendered
-    end
-
-    before_unmount do
-      # cleanup any thing (i.e. timers) before component is destroyed
-    end
-
-    def render
-      div do
-        'Helloworld'
+  render(DIV) do
+    show_button
+    if state.show_field
+      DIV(class: 'formdiv') do
+        show_input
+        H1 { state.field_value }
       end
     end
   end
 
+  def show_button
+    BUTTON(class: 'btn btn-info') do
+      state.show_field ? 'Click to hide HelloWorld input field' : 'Click to show HelloWorld input field'
+    end.on(:click) { mutate.show_field !state.show_field }
+  end
+
+  def show_input
+    H4 do
+      SPAN { 'Please type ' }
+      SPAN(class: 'colored') { 'Hello World' }
+      SPAN { ' in the input field below :' }
+      BR {}
+      SPAN { 'Or anything you want (^̮^)' }
+    end
+
+    INPUT(type: :text, class: 'form-control').on(:change) do |e|
+      mutate.field_value e.target.value
+    end
+  end
+
+  def show_text
+    H1 { state.field_value }
+  end
+
+end
