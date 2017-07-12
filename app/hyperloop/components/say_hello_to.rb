9dbbe7bf0cd87737c18429cf68bfc399c5a5b262
Billy.Zheng <zw963@163.com>
2017-07-12 21:41:56 +0800

    使用 param 来指定属性的类型, 这些设定其实是可选的.

--- app/hyperloop/components/say_hello_to.rb
+++ app/hyperloop/components/say_hello_to.rb
@@ -1,5 +1,6 @@
 class SayHelloTo < Hyperloop::Component
-  param :name, type: :string
+  # 下面的代码是可选的, 但是编译为 js 时会有更好的性能.
+  param :name, type: String
 
   render(DIV) do
     H4 { "Hello #{params.name}!" }
