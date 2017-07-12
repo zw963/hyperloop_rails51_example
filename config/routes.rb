18e09fe08ca7010a1296af1834fae0b95c5272c6
Billy.Zheng <zw963@163.com>
2017-07-12 18:38:16 +0800

    更改为普通的 controller 方式, 功能和之前一样.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount Hyperloop::Engine => '/hyperloop'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'hyperloop#helloworld'
+  # root 'hyperloop#helloworld'
+  root 'home#helloworld'
 end
