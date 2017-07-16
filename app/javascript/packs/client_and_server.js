38c419bdaa9d183d04fe0fc67cc33d62afc27c5c
Billy.Zheng <zw963@163.com>
2017-07-17 00:55:42 +0800

    Fix to make player work.

--- app/javascript/packs/client_and_server.js
+++ app/javascript/packs/client_and_server.js
@@ -1,4 +1,6 @@
 ReactDOM = require('react-dom');
 React = require('react');
 ReactBootstrap = require('react-bootstrap');
-ReactPlayer = require('react-player');
+// 这里添加必须添加 default 才可以工作, 怀疑这是因为:
+// react-player 不是一个标准的组件库, 而是一个独立的组件.
+ReactPlayer = require('react-player').default;
