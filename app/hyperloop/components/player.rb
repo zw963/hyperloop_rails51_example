732827c49bb660d55424e470cc992a123362bef2
Billy.Zheng <zw963@163.com>
2017-07-16 20:11:47 +0800

    Add settings as gitter advice, not worked...

--- app/hyperloop/components/player.rb
+++ app/hyperloop/components/player.rb
@@ -1,9 +1,5 @@
-# class Player < React::Component::Base
-#   imports 'ReactPlayer'
-# end
-
-# class LiftOff < Hyperloop::Component
-#   render(DIV) do
-#     Player(url: 'https://www.youtube.com/embed/Czrc1JfIBRw', playing: false)
-#   end
-# end
+class Player < Hyperloop::Component
+  render(DIV) do
+    ReactPlayer(url: 'https://www.youtube.com/embed/Czrc1JfIBRw', playing: false)
+  end
+end
--- config/initializers/hyperloop.rb
+++ config/initializers/hyperloop.rb
@@ -4,4 +4,5 @@ Hyperloop.configuration do |config|
   config.transport = :simple_poller
   config.import 'client_and_server'
   config.import 'client_only', client_only: true
+  config.import 'reactrb/auto-import'
 end
