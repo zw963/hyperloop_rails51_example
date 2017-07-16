05215ccb9af2a64740f36e59553c3f01a6f56bcc
Billy.Zheng <zw963@163.com>
2017-07-16 13:36:16 +0800

    使用 state 模块重构

--- app/hyperloop/components/off_luck_dip.rb
+++ app/hyperloop/components/off_luck_dip.rb
@@ -1,3 +1,28 @@
+# 我们使用 store 来抽取 state 到一个单独的 Store.
+# 这有利于从 Component 单独抽取 `状态和业务逻辑` 出来.
+
+class Discounter < Hyperloop::Store
+  # 这个 Discounter 即包含状态, 例如: state
+  # 这里的 scope: :class, 表示是一个类方法?
+  state discount: 30, scope: :class, reader: true
+
+  # 也包含修改状态的业务逻辑, 例如: luck_dip!
+  def self.lucky_dip!
+    mutate.discount(state.discount + rand(-5..5))
+  end
+end
+
+class OfferLuckyDip < Hyperloop::Component
+  render(DIV) do
+    # State 中的 state 方法自动定义了一个 reader 类方法.
+    H1 { "Your discount is #{Discounter.discount}%" }
+    BUTTON { 'Lucky Dip' }.on(:click) do
+      # 这里直接调用类方法.
+      Discounter.lucky_dip!
+    end
+  end
+end
+
 class OfferLuckyDip < Hyperloop::Component
   # 这里设定了一个属于这个 Component 的 state.
   # 如果有其他 Component 依赖于这个状态自动刷新,
