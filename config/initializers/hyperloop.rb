59765feac62a3b813cff6b63e8bb4008d49c0caa
Billy.Zheng <zw963@163.com>
2017-07-17 12:29:13 +0800

    transport use action-cable

--- config/initializers/hyperloop.rb
+++ config/initializers/hyperloop.rb
@@ -1,7 +1,7 @@
 #config/initializers/hyperloop.rb
 
 Hyperloop.configuration do |config|
-  config.transport = :simple_poller
+  config.transport = :action_cable
   config.import 'client_and_server'
   config.import 'client_only', client_only: true
   config.import 'reactrb/auto-import'
