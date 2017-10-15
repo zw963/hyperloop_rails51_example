57dbaebe70f2f1e62d9addd30730bd2e8b710d21
Billy.Zheng <zw963@163.com>
2017-10-15 22:47:47 +0800

    启用 opal_hot_reloader

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bundle exec rails s -p 3000 -b 0.0.0.0
+notloader: opal-hot-reloader -d app/hyperloop/components
--- app/hyperloop/components.rb
+++ app/hyperloop/components.rb
@@ -16,7 +16,14 @@ require 'hyper-model'
 require 'hyper-store'
 require 'hyper-operation'
 require 'models'
+require 'hyper-router/react-router-source'
+require 'hyper-router'
 
-require_tree './components'
+require 'opal_hot_reloader'
+# 启用了 opal_hot_reloader, 注意, 对 preloader: true 无效的.
+OpalHotReloader.listen
+
+require_tree './models' if RUBY_ENGINE == 'opal'
 require_tree './operations'
 require_tree './stores'
+require_tree './components'
